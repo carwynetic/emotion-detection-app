82cc4f95d3619e8267be17bffd61627e3b868e0d
Carwyn Nguyen <baote332@gmail.com>
2026-06-17 11:06:16 +0700

    Update mywebscript.js

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,1 +1,19 @@
+function runEmotionAnalysis() {
+    const textToAnalyze = document.getElementById("textToAnalyze").value;
 
+    const request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            document.getElementById("system_response").innerHTML = request.responseText;
+        }
+    };
+
+    request.open(
+        "GET",
+        "/emotionDetector?textToAnalyze=" + encodeURIComponent(textToAnalyze),
+        true
+    );
+
+    request.send();
+}
